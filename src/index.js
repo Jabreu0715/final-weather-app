c3116485125e26b4ee220498b38baec5671a28c9
Jabreu0715 <127450942+Jabreu0715@users.noreply.github.com>
2023-10-05 12:42:29 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 let apiKey = "23ftbd4aecd5fa6f4304ea2800dofdbf";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=London&key=${apiKey}&units=metric`;
-console.log(apiUrl);
+let city = "New York";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(url).then(displayTemperature);
