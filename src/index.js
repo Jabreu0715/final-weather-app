d4e6662e553d7298eff4d5116fd8b07c771e9a3d
Jabreu0715 <127450942+Jabreu0715@users.noreply.github.com>
2023-10-05 15:16:42 -0400

    updated information to include API with JS

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 function displayTemperature(response) {
   console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "23ftbd4aecd5fa6f4304ea2800dofdbf";
