4e315ee43cf1f673fd4e199e8ea8ec9daa4f7b31
Jabreu0715 <127450942+Jabreu0715@users.noreply.github.com>
2023-10-05 12:46:38 -0400

    changed city for apiUrl

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+function displayTemperature(response) {
+  console.log(respose.data);
+}
+
 let apiKey = "23ftbd4aecd5fa6f4304ea2800dofdbf";
 let city = "New York";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
